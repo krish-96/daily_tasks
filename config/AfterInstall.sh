a2c3d622a38e24954a892b07dc60a4e2c1d5901d
gopikrishnab <gopikrishnab@ideyalabs.com>
2023-09-06 10:07:33 +0530

    Modified

--- config/AfterInstall.sh
+++ config/AfterInstall.sh
@@ -7,7 +7,7 @@ aws s3 cp s3://daily-status-app-settings/daily_app_settings.py /home/ubuntu/Dail
 echo "File copied from S3"
 #virtualenv env -p /usr/bin/python3.8
 
-/usr/bin/python3.8 -m venv venv
+/usr/bin/python3 -m venv venv
 
 # Activate virtual env
 source venv/bin/activate
