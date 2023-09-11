de1372712e43fe8282dad188991beac0c2a995d1
gopikrishnab <gopikrishnab@ideyalabs.com>
2023-09-11 08:36:08 +0530

    Modified settigns

--- daily_status_proj/settings.py
+++ daily_status_proj/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "staticfiles",
-]
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
