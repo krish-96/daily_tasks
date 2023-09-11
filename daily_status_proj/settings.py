917defbb45f44d4a5554062f829f11ac4ab9462e
gopikrishnab <gopikrishnab@ideyalabs.com>
2023-09-11 08:34:38 +0530

    Modified settigns

--- daily_status_proj/settings.py
+++ daily_status_proj/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles",
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
